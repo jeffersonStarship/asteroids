c4989627cf04bb5c74ce664c368b35bd79c2ca3d
ensarkara13 <ensarkara13@gmail.com>
2024-10-25 23:56:07 +0300

    created necessary modules

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    pygame.init()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for d in drawable:
+            d.update(dt)
+        
         screen.fill("black")
+
+        for u in updatable:
+            u.update(dt)
+
+        player.draw(screen)
         pygame.display.flip()
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
